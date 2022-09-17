d448938c00eadc6b77249f7407885dd2223787fc
Andrei <andreisolcan@protonmail.com>
2022-09-17 18:25:25 +0300

    s5

---
+++ 05-Design/test.js
@@ -0,0 +1,17 @@
+// let animals = 10;
+// let cows = 2;
+// let horses = 2;
+
+// function egsNumber(animals, cows, horses) {
+//     return animals - (cows + horses);
+// }
+
+// let egs = function() {
+//     animals: 10,
+//     cows: 2,
+//     horses: 2,
+//     egsNumber: function() {
+//         return this.animals - (this.cows + this.horses);
+//     }
+// };
+// egs.egsNumber();
